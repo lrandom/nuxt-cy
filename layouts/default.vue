eaeb9371942acdabbaca23e8437bfdda99b05174
Nguyen Thanh Luan <luann4099@gmail.com>
2022-04-15 08:32:25 +0700

    update demo

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav>Nav Menu</nav>
+    <Nuxt></Nuxt>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent"
+}
+</script>
+
